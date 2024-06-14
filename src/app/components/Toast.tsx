0caa992470de74370a3ede7cdcff24a219be1e1e
yurii_heivakh <yurii.heivakh@khealth.com>
2024-06-14 11:29:57 +0300

    toast

---
+++ src/app/components/Toast.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+interface ToastProps {
+    type: 'warning' | 'info';
+    message: string;
+    onClose: () => void;
+}
+
+const Toast: React.FC<ToastProps> = ({ type, message, onClose }) => {
+  const icons = {
+    warning: (
+      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
+      </svg>
+    ),
+    info: (
+      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 2a8 8 0 1 0 8 8 8 8 0 0 0-8-8Zm1 12H9v-1h1v-4H9v-1h2v5Zm0-8H9V5h2v1Z"/>
+      </svg>
+    ),
+    // Add more icons as needed
+  };
+
+  const bgColor = {
+    warning: 'bg-orange-100 dark:bg-orange-700',
+    info: 'bg-blue-100 dark:bg-blue-700',
+    // Add more colors as needed
+  };
+
+  const textColor = {
+    warning: 'text-orange-500 dark:text-orange-200',
+    info: 'text-blue-500 dark:text-blue-200',
+    // Add more colors as needed
+  };
+
+  return (
+    <div className={`flex mt-2 mb-2 items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 ${bgColor[type]}`} role="alert">
+      <div className={`inline-flex items-center justify-center flex-shrink-0 w-8 h-8 ${textColor[type]} rounded-lg`}>
+        {icons[type]}
+        <span className="sr-only">{type} icon</span>
+      </div>
+      <div className="ms-3 text-sm font-normal">{message}</div>
+      <button
+        type="button"
+        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        onClick={onClose}
+        aria-label="Close"
+      >
+        <span className="sr-only">Close</span>
+        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Toast;
--- src/app/components/create-user.tsx
+++ src/app/components/create-user.tsx
@@ -1,28 +1,27 @@
 "use client";
 
 import { redirect, useRouter } from "next/navigation";
-import { useState, type FormEvent } from "react";
-import { useToast, Select, Input } from '@chakra-ui/react'
+import { useState } from "react";
 
 import { api } from "@goober/trpc/react";
 import { UserRole } from "@prisma/client";
-import { setIndexedDBUser, setLocalStorage } from "../utils";
+import { setIndexedDBUser } from "../utils";
 import { useCurrentUser } from "../hooks/useUser";
 import { COMMON_ERROR_MESSAGE } from "../constants";
+import Toast from "./Toast";
 
 export function CreateUser() {
   const router = useRouter();
   const [name, setName] = useState("");
   const [role, setRole] = useState<UserRole>(UserRole.RIDER);
-  const toast = useToast()
-
-  // const user = useCurrentUser();
-  // if (user?.id) {
-  //   return redirect('/')
-  // }
-  // console.warn(user, 'user')
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
 
   const createPost = api.users.create.useMutation({
+    onMutate: () => {
+      setToastMessage('')
+      setShowToast(false)
+    },
     onSuccess: async (user) => {
       router.refresh();
       setName("");
@@ -31,41 +30,49 @@ export function CreateUser() {
       await setIndexedDBUser(userData);
     },
     onError: (error) => {
-      toast({
-        title: 'Uh-oh!',
-        description: error?.[0]?.message || COMMON_ERROR_MESSAGE,
-      })
+      const errorParsed = JSON.parse(error.message)
+      setToastMessage(errorParsed?.[0]?.message || COMMON_ERROR_MESSAGE)
+      setShowToast(true)
     }
   });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleCloseToast = () => {
+    setShowToast(false);
+  };
+
+  const handleSubmit = async () => {
     createPost.mutate({ name, role });
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col gap-2 absolute top-4 left-4 z-20"
-    >
-      <Input
-        type="text"
-        placeholder="Title"
+    <div className="absolute top-4 left-4 z-50 w-[300px] max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <input 
+        type="text" 
+        id="name" 
         value={name}
+        className="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        placeholder="Yurii" 
         onChange={(e) => setName(e.target.value)}
-        // className="w-full rounded-full px-4 py-2 text-black"
+        required 
       />
-      <Select name="role" onChange={(e) => setRole(e.target.value as UserRole)}>
+      <select id="role" name="role" onChange={(e) => setRole(e.target.value as UserRole)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option value={UserRole.RIDER}>Rider</option>
         <option value={UserRole.DRIVER}>Driver</option>
-      </Select>
+      </select>
+      {showToast && (
+        <Toast
+          type="warning"
+          message={toastMessage}
+          onClose={handleCloseToast}
+        />
+      )}
       <button
-        type="submit"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+        className="text-gray-900 w-full mt-4 cursor-pointer bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
         disabled={!name || createPost.isPending}
+        onClick={handleSubmit}
       >
         {createPost.isPending ? "Submitting..." : "Submit"}
       </button>
-    </form>
+    </div>
   );
 }
